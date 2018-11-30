aeba3b57d76af95725049651a783ec4f8c20b90b
Manuel Alejandro Matus de Quevedo <manuel.matus@linio.com>
2018-11-30 00:28:29 -0600

    Second Commit

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -1,24 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
 /*
-* Use this component as a launching-pad to build your functionality.
-*
-*/
-class YourComponent extends Component {
+ * Use this component as a launching-pad to build your functionality.
+ *
+ */
+
+const mapStyles = {
+  width: "80%",
+  height: "90%"
+};
+
+export class MexicoCityMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: []
+    };
+  }
+
+  componentDidUpdate() {
+    const google = window.google;
+
+    this.map = new google.maps.Map(this.refs.map, {
+      center: this.props.center,
+      zoom: 4
+    });
+
+    this.createMarkers(this.props.markers);
+  }
+
+  createMarkers(users) {
+    const google = window.google;
+
+    users.map(user => {
+      this.marker = new google.maps.Marker({
+        position: {
+          lat: user.location.latitude,
+          lng: user.location.longitude
+        },
+        map: this.map
+      });
+      this.state.markers.push(this.marker);
+    });
+  }
+
   render() {
     return (
-      <div style={divStyle}>
-        <h1> Put your solution here!</h1>
+      <div className="container justify-content-center">
+        <Map
+          google={this.props.google}
+          zoom={11}
+          style={mapStyles}
+          initialCenter={{
+            lat: 19.433376,
+            lng: -99.141514
+          }}
+        >
+          <Marker onClick={this.onMarkerClick} name={"Testing Purposes"} />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          />
+        </Map>
       </div>
     );
   }
 }
 
-var divStyle = {
-  border: 'red',
-  borderWidth: 2,
-  borderStyle: 'solid',
-  padding: 20
-};
-
-export default YourComponent;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A"
+})(MexicoCityMap);
